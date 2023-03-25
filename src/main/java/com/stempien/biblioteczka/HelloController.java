1a95d20629060157a657367b29c7d23f9745237d
Ja <adik08.email@gmail.com>
2023-03-25 13:06:01 +0100

    1. ekran logowania

--- src/main/java/com/stempien/biblioteczka/HelloController.java
+++ src/main/java/com/stempien/biblioteczka/HelloController.java
@@ -2,13 +2,19 @@ package com.stempien.biblioteczka;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public TextField txtUser;
+    public TextField txtPassword;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onButtonClick() {
+        String password = txtPassword.getText();
+        String login = txtUser.getText();
+        if(login.equals("Bob") && password.equals("Bob")){
+
+        }
     }
 }
